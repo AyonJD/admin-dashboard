f441d15b577873bf2270bfa0ce95ab9c17a032ec
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-02-16 13:50:04 +0600

    Sidebar icon

--- src/partials/BreadCrumb/BreadCrumb.jsx
+++ src/partials/BreadCrumb/BreadCrumb.jsx
@@ -4,7 +4,7 @@ const Breadcrumb = ({ title, userName }) => {
     return (
         <div className="breadcrumb flex items-center px-4 mb-2 pt-4">
             <h1 className='text-sm font-medium text-gray-500 '> {title} </h1>
-            <h1 className='text-xl font-medium text-gray-500 mx-2'> {`>`} </h1>
+            <h1 className='text-sm font-medium text-gray-400 mx-2'> {`>`} </h1>
             <h1 className='text-sm font-medium text-gray-500 '> {userName} </h1>
 
         </div>
